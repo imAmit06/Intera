b76ed465d9a0a1e3fa87a90e1de7eca160dd2292
Amit Patil <74283636+imAmit06@users.noreply.github.com>
2026-07-06 12:51:36 +0530

    feat: implement conditional rendering for sign-in and sign-up buttons based on user authentication state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import "./App.css";
-import { SignInButton, SignUpButton } from "@clerk/react";
+import { Show, UserButton, SignInButton, SignUpButton } from "@clerk/react";
 
 function App() {
   return (
     <>
       <h1>Welcome to Intera</h1>
-      <SignInButton mode="modal" />
-      <SignUpButton mode="modal" />
+      <Show when="signed-out">
+        <SignInButton mode="modal" />
+        <SignUpButton mode="modal" />
+      </Show>
+      <Show when="signed-in">
+        <UserButton mode="modal" />
+      </Show>
     </>
   );
 }
