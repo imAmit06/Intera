66f48e33c89fb6831becf771e7cf89164e115929
Amit Patil <74283636+imAmit06@users.noreply.github.com>
2026-07-16 16:48:44 +0530

    feat: add publishable key to ClerkProvider for authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App.jsx";
 import { ClerkProvider } from "@clerk/react";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const queryClient = new QueryClient();
 
@@ -12,7 +13,7 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ClerkProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
         </ClerkProvider>
       </QueryClientProvider>
